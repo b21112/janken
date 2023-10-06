ef2b70bb226f789bc15caddef073246e44613993
b21112 <e1b21112@oit.ac.jp>
2023-10-07 00:24:08 +0900

    実装完了

--- src/main/java/oit/is/z1828/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z1828/kaizi/janken/controller/JankenController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class JankenController {
@@ -17,6 +18,26 @@ public class JankenController {
   @PostMapping("/janken")
   public String Janken(@RequestParam String name, ModelMap model) {
     model.addAttribute("name", name);
+    return "janken.html";
+  }
+
+  @GetMapping("/JankenGame/{hand}")
+  public String JankenGame(@PathVariable String hand, ModelMap model) {
+
+    String result;
+    String yourHand = hand;
+    String cpuHand = "gu";
+
+    if (yourHand.equals("pa")) {
+      result = "win";
+    } else if (yourHand.equals("gu")) {
+      result = "draw";
+    } else {
+      result = "lose";
+    }
+    model.addAttribute("yourHand", yourHand);
+    model.addAttribute("cpuHand", cpuHand);
+    model.addAttribute("result", result);
     return "janken.html";
   }
 }
