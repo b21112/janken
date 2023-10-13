e1f4995edb9d314967cd2c0a19a550e4017a8085
b21112 <e1b21112@oit.ac.jp>
2023-10-13 18:44:31 +0900

    ログイン機能の実装

--- src/main/java/oit/is/z1828/kaizi/janken/model/Janken.java
+++ src/main/java/oit/is/z1828/kaizi/janken/model/Janken.java
@@ -1,5 +1,54 @@
 package oit.is.z1828.kaizi.janken.model;
 
+import java.util.Random;
+
 public class Janken {
-  
+
+  public static String judgeGame(String yourHand, String cpuHand) {
+    if (yourHand.equals(cpuHand)) {
+      return "draw";
+    }
+    if (yourHand.equals("グー")) {
+      if (cpuHand.equals("チョキ")) {
+        return "win";
+      }
+    }
+    if (yourHand.equals("チョキ")) {
+      if (cpuHand.equals("パー")) {
+        return "win";
+      }
+    }
+    if (yourHand.equals("パー")) {
+      if (cpuHand.equals("グー")) {
+        return "win";
+      }
+    }
+
+    return "lose";
+  }
+
+  public static String setYourHand(String hand) {
+    switch (hand) {
+      case ("gu"):
+        return "グー";
+      case ("choki"):
+        return "チョキ";
+      default:
+        return "パー";
+    }
+  }
+
+  public static String setCpuHand() {
+    Random random = new Random();
+    int handSeed = random.nextInt(3);
+
+    switch (handSeed) {
+      case (0):
+        return "グー";
+      case (1):
+        return "チョキ";
+      default:
+        return "パー";
+    }
+  }
 }
