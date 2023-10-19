fa13379a067e7ba5ca98bd4ac6be226441b56fa2
b21112 <e1b21112@oit.ac.jp>
2023-10-19 15:06:15 +0900

    test

--- src/main/java/oit/is/z1828/kaizi/janken/model/UserMapper.java
+++ src/main/java/oit/is/z1828/kaizi/janken/model/UserMapper.java
@@ -8,8 +8,8 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface UserMapper {
 
-  @Select("SELECT * from users where id = #{id}")
-  User selectById(int id);
+  /*@Select("SELECT * from users where id = #{id}")
+  User selectById(int id);*/
 
   @Select("SELECT * from users")
   ArrayList<User> selectAllByUsers();
