0a471e7fcd1eb2904209abcefa1a49ba0c387cac
b21112 <e1b21112@oit.ac.jp>
2023-10-21 18:27:37 +0900

    試合結果の表示と試合ページの移動までを実装した

--- src/main/java/oit/is/z1828/kaizi/janken/model/UserMapper.java
+++ src/main/java/oit/is/z1828/kaizi/janken/model/UserMapper.java
@@ -8,9 +8,10 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface UserMapper {
 
-  /*@Select("SELECT * from users where id = #{id}")
-  User selectById(int id);*/
+  @Select("SELECT * from users where id = #{id}")
+  User selectById(int id);
 
   @Select("SELECT * from users")
   ArrayList<User> selectAllByUsers();
+
 }
